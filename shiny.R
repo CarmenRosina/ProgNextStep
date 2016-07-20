6f0de78188757c7ec2938c516ba3adce5153487d
CarmenRosina <carmen.wolvius@gmail.com>
2016-07-20 14:13:35 +0200

    Simple example of a shiny app. no ui.R or server.R necessary; shinyApp() combines the files

---
+++ shiny.R
@@ -0,0 +1,11 @@
+#install.packages("shiny")
+library(shiny)
+
+ui <- fluidPage(sliderInput(inputId = "num",
+                            label = "Choose a number",
+                            value = 25, min = 1, max = 100)
+)
+
+server <- function(input, output){}
+
+shinyApp(ui=ui, server=server)
